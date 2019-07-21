ff06aa1bbebcb668580a3f468fd139330872f5e1
Bruno Cremaschi <bscremaschi@gmail.com>
2019-07-21 19:55:38 -0300

    Este commit implementa as rotas para chamada dos componentes NewProductView e ProductListView

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import NewProductView from './views/NewProductView';
 import ProductListView from './views/ProductListView';
@@ -7,8 +8,24 @@ import ProductListView from './views/ProductListView';
 function App() {
   return (
     <div className="App">
-      <NewProductView />
-      <ProductListView />
+      <Router>
+        <div>
+          <header>
+            <ul className="link-list">
+              <li>
+                <Link to={'/'} >Novo</Link>
+              </li>
+              <li>
+                <Link to={'/list'} >Lista</Link>
+              </li>
+            </ul>
+          </header>
+          <div>
+            <Route path={'/'} exact component={NewProductView} ></Route>
+            <Route path={'/list'} exact component={ProductListView} ></Route>
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
